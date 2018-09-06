5098443832aea58d52138ea47f4c675c8fc86405
AFlear <aflear@yandex.ru>
2018-09-06 20:51:20 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Sidebar from "react-sidebar";
-
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
 
 const styles = {
     sidebar: {
@@ -28,22 +28,75 @@ const styles = {
 };
 
 
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            sidebarOpen: true
+            sidebarOpen: true,
+            results:{
+                "inputData":0,
+            },
+            data:[
+                {"failed":[]},
+                {"success":[]}
+            ],
+            inputValue:0
         };
         this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.sieveOfEratosthenes = this.sieveOfEratosthenes.bind(this);
     }
 
     onSetSidebarOpen(open) {
         this.setState({ sidebarOpen: open });
     }
+    handleClick() {
+        this.sieveOfEratosthenes(this.state.inputValue)
+    }
+    handleChange(e) {
+        this.setState({inputValue:e.target.value})
+    }
+
+
+
+    sieveOfEratosthenes(n) {
+        let primes = [];
+        for (let i = 2; i <= n; i++) {
+            primes[i] = true;
+        }
+        let p = 2;
+        do {
+            for (let i = 2 * p; i <= n; i += p) {
+                primes[i] = false;
+            }
+            for (let i = p; i <= n; i++) {
+                if (primes[i] && i > p) {
+                    p = i;
+                    break;
+                }
+            }
+        } while (p * p < n);
+        let primesValues = [];
+        let primesFailes = [];
+        for (let i = 2; i <= n; i++) {
+            if (primes[i]) {
+                primesValues.push(i);
+            }else{
+                primesFailes.push(i);
+            }
+        }
+        this.setState({"data":[{'success':primesValues},{'failed':primesFailes}]});
+        console.log(this.state.data)
+    }
   render() {
       const links = [];
-
+      const columns = [{
+          dataField: 'failed',
+          text: 'Product Name'
+      }, {
+          dataField: 'success',
+          text: 'Product Price'
+      }];
       for (let ind = 0; ind < 10; ind++) {
           links.push(
               <a key={ind} href="#" style={styles.sidebarLink}>
@@ -55,7 +108,7 @@ class App extends Component {
       <div className="App">
           <Sidebar
               sidebar={links}
-              docked = 'true'
+              docked = {true}
               open={this.state.sidebarOpen}
               onSetOpen={this.onSetSidebarOpen}
               styles={{ sidebar: { background: "white" } }}
@@ -97,6 +150,22 @@ class App extends Component {
                   return [x for x in range(n) if booleanSieve[x]]
                     `}
             </pre>
+          <input
+              type="number"
+              value={this.state.inputValue}
+              onChange={ this.handleChange.bind(this) }
+              placeholder="Input number" />
+
+          <button
+              className=""
+              onClick={ this.handleClick.bind(this, this.state.results.inputValue)}>Запустить</button>
+          {/*<BootstrapTable keyField='id' data={ this.state.data } columns={ columns } />*/}
+          <div>
+              Not prime :
+              {this.state.data.success.map((succ)=>{
+
+              })}
+          </div>
       </div>
     );
   }
